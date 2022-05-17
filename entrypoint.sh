c55dab20692d512bb05da2bcc17be65bed3a214b
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-05-17 20:48:51 +1000

    echo scan variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,7 +52,7 @@ scan_output="$(driftctl scan $qflag $INPUT_ARGS)"
 # Escape scan output to handle multilines
 scan_output="${scan_output//$'\n'/'%0A'}"
 
-printf $scan_output
+echo "$scan_output"
 
 # Set output to be used for other Github Actions jobs
 echo "::set-output name=driftctl::$scan_output"

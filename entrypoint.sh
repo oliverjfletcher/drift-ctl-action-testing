3037967c36c696e8c1606efd5ad881ba2f06c0bd
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-06-11 10:13:16 +1000

    driftctl gh action testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,6 +66,6 @@ scan_output(){
 scan_output $val1
 
 # Set output to be used for other Github Actions jobs
-echo "::set-output name=driftctl::$scan_output"
+echo ::set-output name=driftctl::$(echo $scan_output)
 
 

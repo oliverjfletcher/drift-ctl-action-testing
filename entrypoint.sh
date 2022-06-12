988c427e943fb8477ea3aa3cbef744c861e16f2e
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-06-13 01:16:33 +1000

    action testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,5 +64,7 @@ scan_output(){
 # Run scan function and store in variable
 scan_output=$(scan_output)
 
+echo -e "$scan_output"
+
 # Set output to be used for other Github Actions jobs
 echo "::set-output name=driftctl::$scan_output"

778f85068b17012327212362aaa70b294dba6e70
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-06-12 21:50:08 +1000

    action testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,6 +73,8 @@ exit_code
 # Echo scan output to Github Actions runner console
 echo -e "$scan_output"
 
+scan_output="${scan_output//$'\r'/'%0D'}"
+
 # Set output to be used for other Github Actions jobs
 echo "::set-output name=driftctl::$scan_output"
 

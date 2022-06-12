a582648a783b42cc810f450fcc2a758e31928da4
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-06-12 19:31:52 +1000

    action testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,9 +51,8 @@ scan_output(){
     scan_output="$(driftctl scan $qflag $INPUT_ARGS;return)"
     exit_code=$?
     return $exit_code
-    scan_output="${scan_output//$'\n'/'%0A'}"
-    echo -e "$scan_output"
 }
+
 # Run scan function 
 scan_output=$(scan_output)
 
@@ -65,6 +64,9 @@ exit_code(){
 }
 # Run exit code function 
 exit_code
+
+scan_output="${scan_output//$'\n'/'%0A'}"
+echo -e "$scan_output"
 
 # Set output to be used for other Github Actions jobs
 echo "::set-output name=driftctl::$scan_output"

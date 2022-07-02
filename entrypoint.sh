d5a9a4058cbc0c3b80bcc6dc19140a34797b6dfe
Oliver Fletcher <oliverjamesfletcher@gmail.com>
2022-07-02 10:04:33 +1000

    gh action testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,9 +52,9 @@ scan_output(){
   scan_output="$(driftctl scan $qflag $INPUT_ARGS;return)"
     # scan_output="${scan_outpu//$'\n'/\\n}"
     # scan_output="${scan_output//'%'/'%25'}"
-    # scan_output="${scan_output//$'\r'/'%0D'}"
     scan_output="${scan_output//$'\n'/'%0A'}"
-    echo -e "$scan_output"
+    # scan_output="${scan_output//$'\r'/'%0D'}"
+    echo $scan_output
   exit_code=$?
   return $exit_code
 }

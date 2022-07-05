35d17ad380327282d27bdf06f6082bfd4e7f457f
Oliver Fletcher <36835356+oliverjfletcher@users.noreply.github.com>
2022-07-05 23:06:23 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,13 +63,13 @@ scan_exit=$?
 
 # Check exit code, fail job if scan command exit code 2, then format scan output for GitHub comment
 scan_exit_code(){
-  if [[ "$scan_exit" -eq 1 || "$scan_exit" -eq 2 ]]; then
+  if [ "$scan_exit" -eq 0]; then
     echo -e "$scan_output"
     scan_output="${scan_output//$'\n'/'%0A'}"
-    exit 1
   else
     echo -e "$scan_output"
     scan_output="${scan_output//$'\n'/'%0A'}"
+    exit 1
   fi
 }
 

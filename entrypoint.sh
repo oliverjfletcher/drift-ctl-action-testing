4fb3a33d52822ae0dff08f2d26810e28a7324624
oliverjfletcher <oliver.fletcher@rackspace.com>
2022-10-25 22:22:16 +1100

    fixing output formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ quiet_flag
 scan_output(){
   scan_output="$(driftctl scan $qflag $INPUT_ARGS;return)"
   scan_exit=$?
-  scan_output="${scan_output//$'\r'/'%0D'}"
+  # scan_output="${scan_output//$'\n'/'%0A'}"
   echo -e "$scan_output"
   return $scan_exit
 }
@@ -65,12 +65,12 @@ scan_exit=$?
 scan_exit_code(){
   if [[ "$scan_exit" -eq 1 || "$scan_exit" -eq 2 ]]; then
     echo -e "$scan_output"
-    scan_output="${scan_output//$'\n'/'%0A'}"
+    # scan_output="${scan_output//$'\n'/'%0A'}"
     echo "driftctl=$scan_output" >> $GITHUB_OUTPUT
     exit 1
   else
     echo -e "$scan_output"
-    scan_output="${scan_output//$'\n'/'%0A'}"
+    # scan_output="${scan_output//$'\n'/'%0A'}"
     echo "driftctl=$scan_output" >> $GITHUB_OUTPUT
   fi
 }
